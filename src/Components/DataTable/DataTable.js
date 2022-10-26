7004d52f10b2fd75f219bd35f371fdc3b4958e22
tronexis <tronexis>
2022-10-26 21:26:46 +0600

    fix: make name column sticky on horizontal scroll

--- src/Components/DataTable/DataTable.js
+++ src/Components/DataTable/DataTable.js
@@ -1,174 +1,246 @@
-import React, { useState } from 'react';
-import TableRow from './TableRow';
-import pomelo from '../../images/pomelo.png';
-
-
+import React, { useState } from "react";
+import TableRow from "./TableRow";
+import pomelo from "../../images/pomelo.png";
 
 const DataTable = ({ dark, coinData, orderBy, setOrderBy }) => {
-
-    const [stared, setStared] = useState([]);
-    const handleStared = (data) => {
-        const isStared = stared.find(item => item.id === data.id);
-        if (isStared){
-            const remainingData = stared.filter(item => item.id !== data.id);
-            setStared(remainingData);
-        } else {
-            setStared(previousValue => [...previousValue, data]);
-        }
+  const [stared, setStared] = useState([]);
+  const handleStared = (data) => {
+    const isStared = stared.find((item) => item.id === data.id);
+    if (isStared) {
+      const remainingData = stared.filter((item) => item.id !== data.id);
+      setStared(remainingData);
+    } else {
+      setStared((previousValue) => [...previousValue, data]);
     }
+  };
 
-    return (
-        // px-5
-        <div className='md:px-[3rem] px-[0.5rem]'  a href="https://t.me/jointrustswap">
-           <a href="https://pomelo.io/grants/trustswap">
-           <div className='bg-[#FBF0DF] gap-10 flex items-center justify-center py-[8px] lg:w-max px-5 mt-[3rem] ml-auto mt-[4.25rem] rounded'>
-                <img className='w-[150px]' src={pomelo} alt="pomelo" />
-                <span className='text-[#021a50] text-[20px]'>Support us on Pomelo season 4!</span>
-           </div>
-           </a>
-            <h3 className='text-3xl mb-5 mt-[-7.75rem] pl-[-0.57rem] Antelope'>Antelope market capitalization</h3>
-
-            <div className="dropdown dropdown-end mb-[2rem] brd">
-                <ul className="menu menu-horizontal p-0 mx-5">
-                  <li>
-                    <a href="##" className='dropdown dd'>
-                      {orderBy}
-                      <svg
-                        className="fill-current"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                      </svg>
-                    </a>
-                    <ul className="p-2 bg-base-100 z-40 ddm">
-                      <li onClick={() => setOrderBy('Order By Liquidity')}>
-                        <a href="##">Order By Liquidity</a>
-                      </li>
-                      <li onClick={() => setOrderBy('Order By MarketCap')}>
-                        <a href="##">Order By MarketCap</a>
-                      </li>
-                      <li onClick={() => setOrderBy('Order By Price')}>
-                        <a href="##">Order By Price</a>
-                      </li>
-                      <li onClick={() => setOrderBy('Order By Supply')}>
-                        <a href="##">Order By Supply</a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-              </div>
-
-            <div className="overflow-x-auto">
-
-                { stared.length > 0 && <table className="table w-full z-0">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>NAME</th>
-                            <th>PRICE</th>
-                            <th>1H</th>
-                            <th>24H</th>
-                            <th>7D</th>
-                            <th>LIQUIDITY</th>
-                            <th>MARKET CAP</th>
-                            <th>SUPPLY</th>
-                            <th>&nbsp;</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                        {
-                            orderBy === "Order By Liquidity" &&
-                            stared?.sort((a, b) => b.liquidity.replace(/,/g, '') - a.liquidity.replace(/,/g, ''))?.map((data, index) =>
-
-                                <TableRow data={data} key={index} index={index} dark={dark} handleStared={handleStared} stared={stared}/>
-                            )
-                        }
-
-                        {
-                            orderBy === "Order By MarketCap" &&
-                            stared?.sort((a, b) => b.marketCap.replace(/,/g, '') - a.marketCap.replace(/,/g, ''))?.map((data, index) =>
-
-                                <TableRow data={data} key={index} index={index} dark={dark} handleStared={handleStared} stared={stared}/>
-                            )
-                        }
-
-                        {
-                            orderBy === "Order By Price" &&
-                            stared?.sort((a, b) => b.price - a.price)?.map((data, index) =>
-
-                                <TableRow data={data} key={index} index={index} dark={dark} handleStared={handleStared} stared={stared}/>
-                            )
-                        }
-
-                        {
-                            orderBy === "Order By Supply" &&
-                            stared?.sort((a, b) => b.supply.replace(/,/g, '') - a.supply.replace(/,/g, ''))?.map((data, index) =>
-
-                                <TableRow data={data} key={index} index={index} dark={dark} handleStared={handleStared} stared={stared}/>
-                            )
-                        }
-                    </tbody>
-                </table>
-
-                }
-
-                <table className="table w-full z-0">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>NAME</th>
-                            <th>PRICE</th>
-                            <th>1H</th>
-                            <th>24H</th>
-                            <th>7D</th>
-                            <th>LIQUIDITY</th>
-                            <th>MARKET CAP</th>
-                            <th>SUPPLY</th>
-                            <th>&nbsp;</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                        {
-                            orderBy === "Order By Liquidity" &&
-                            coinData?.sort((a, b) => b.liquidity.replace(/,/g, '') - a.liquidity.replace(/,/g, ''))?.map((data, index) =>
-
-                                <TableRow data={data} key={index} index={index} dark={dark} handleStared={handleStared} stared={stared}/>
-                            )
-                        }
-
-                        {
-                            orderBy === "Order By MarketCap" &&
-                            coinData?.sort((a, b) => b.marketCap.replace(/,/g, '') - a.marketCap.replace(/,/g, ''))?.map((data, index) =>
-
-                                <TableRow data={data} key={index} index={index} dark={dark} handleStared={handleStared} stared={stared}/>
-                            )
-                        }
-
-                        {
-                            orderBy === "Order By Price" &&
-                            coinData?.sort((a, b) => b.price - a.price)?.map((data, index) =>
-
-                                <TableRow data={data} key={index} index={index} dark={dark} handleStared={handleStared} stared={stared}/>
-                            )
-                        }
-
-                        {
-                            orderBy === "Order By Supply" &&
-                            coinData?.sort((a, b) => b.supply.replace(/,/g, '') - a.supply.replace(/,/g, ''))?.map((data, index) =>
-
-                                <TableRow data={data} key={index} index={index} dark={dark} handleStared={handleStared} stared={stared}/>
-                            )
-                        }
-                    </tbody>
-                </table>
-            </div>
+  return (
+    // px-5
+    <div
+      className="md:px-[3rem] px-[0.5rem]"
+      a
+      href="https://t.me/jointrustswap"
+    >
+      <a href="https://pomelo.io/grants/trustswap">
+        <div className="bg-[#FBF0DF] gap-10 flex items-center justify-center py-[8px] lg:w-max px-5 mt-[3rem] ml-auto mt-[4.25rem] rounded">
+          <img className="w-[150px]" src={pomelo} alt="pomelo" />
+          <span className="text-[#021a50] text-[20px]">
+            Support us on Pomelo season 4!
+          </span>
         </div>
-    );
+      </a>
+      <h3 className="text-3xl mb-5 mt-[-7.75rem] pl-[-0.57rem] Antelope">
+        Antelope market capitalization
+      </h3>
+
+      <div className="dropdown dropdown-end mb-[2rem] brd">
+        <ul className="menu menu-horizontal p-0 mx-5">
+          <li>
+            <a href="##" className="dropdown dd">
+              {orderBy}
+              <svg
+                className="fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+              >
+                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+              </svg>
+            </a>
+            <ul className="p-2 bg-base-100 z-40 ddm">
+              <li onClick={() => setOrderBy("Order By Liquidity")}>
+                <a href="##">Order By Liquidity</a>
+              </li>
+              <li onClick={() => setOrderBy("Order By MarketCap")}>
+                <a href="##">Order By MarketCap</a>
+              </li>
+              <li onClick={() => setOrderBy("Order By Price")}>
+                <a href="##">Order By Price</a>
+              </li>
+              <li onClick={() => setOrderBy("Order By Supply")}>
+                <a href="##">Order By Supply</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+
+      <div className="overflow-x-auto">
+        {stared.length > 0 && (
+          <table className="table w-full z-0">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th className="sticky left-16">NAME</th>
+                <th>PRICE</th>
+                <th>1H</th>
+                <th>24H</th>
+                <th>7D</th>
+                <th>LIQUIDITY</th>
+                <th>MARKET CAP</th>
+                <th>SUPPLY</th>
+                <th>&nbsp;</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orderBy === "Order By Liquidity" &&
+                stared
+                  ?.sort(
+                    (a, b) =>
+                      b.liquidity.replace(/,/g, "") -
+                      a.liquidity.replace(/,/g, "")
+                  )
+                  ?.map((data, index) => (
+                    <TableRow
+                      data={data}
+                      key={index}
+                      index={index}
+                      dark={dark}
+                      handleStarred={handleStared}
+                      stared={stared}
+                    />
+                  ))}
+
+              {orderBy === "Order By MarketCap" &&
+                stared
+                  ?.sort(
+                    (a, b) =>
+                      b.marketCap.replace(/,/g, "") -
+                      a.marketCap.replace(/,/g, "")
+                  )
+                  ?.map((data, index) => (
+                    <TableRow
+                      data={data}
+                      key={index}
+                      index={index}
+                      dark={dark}
+                      handleStarred={handleStared}
+                      stared={stared}
+                    />
+                  ))}
+
+              {orderBy === "Order By Price" &&
+                stared
+                  ?.sort((a, b) => b.price - a.price)
+                  ?.map((data, index) => (
+                    <TableRow
+                      data={data}
+                      key={index}
+                      index={index}
+                      dark={dark}
+                      handleStarred={handleStared}
+                      stared={stared}
+                    />
+                  ))}
+
+              {orderBy === "Order By Supply" &&
+                stared
+                  ?.sort(
+                    (a, b) =>
+                      b.supply.replace(/,/g, "") - a.supply.replace(/,/g, "")
+                  )
+                  ?.map((data, index) => (
+                    <TableRow
+                      data={data}
+                      key={index}
+                      index={index}
+                      dark={dark}
+                      handleStarred={handleStared}
+                      stared={stared}
+                    />
+                  ))}
+            </tbody>
+          </table>
+        )}
+
+        <table className="table w-full z-0">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th className="sticky left-16">NAME</th>
+              <th>PRICE</th>
+              <th>1H</th>
+              <th>24H</th>
+              <th>7D</th>
+              <th>LIQUIDITY</th>
+              <th>MARKET CAP</th>
+              <th>SUPPLY</th>
+              <th>&nbsp;</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orderBy === "Order By Liquidity" &&
+              coinData
+                ?.sort(
+                  (a, b) =>
+                    b.liquidity.replace(/,/g, "") -
+                    a.liquidity.replace(/,/g, "")
+                )
+                ?.map((data, index) => (
+                  <TableRow
+                    data={data}
+                    key={index}
+                    index={index}
+                    dark={dark}
+                    handleStarred={handleStared}
+                    stared={stared}
+                  />
+                ))}
+
+            {orderBy === "Order By MarketCap" &&
+              coinData
+                ?.sort(
+                  (a, b) =>
+                    b.marketCap.replace(/,/g, "") -
+                    a.marketCap.replace(/,/g, "")
+                )
+                ?.map((data, index) => (
+                  <TableRow
+                    data={data}
+                    key={index}
+                    index={index}
+                    dark={dark}
+                    handleStarred={handleStared}
+                    stared={stared}
+                  />
+                ))}
+
+            {orderBy === "Order By Price" &&
+              coinData
+                ?.sort((a, b) => b.price - a.price)
+                ?.map((data, index) => (
+                  <TableRow
+                    data={data}
+                    key={index}
+                    index={index}
+                    dark={dark}
+                    handleStarred={handleStared}
+                    stared={stared}
+                  />
+                ))}
+
+            {orderBy === "Order By Supply" &&
+              coinData
+                ?.sort(
+                  (a, b) =>
+                    b.supply.replace(/,/g, "") - a.supply.replace(/,/g, "")
+                )
+                ?.map((data, index) => (
+                  <TableRow
+                    data={data}
+                    key={index}
+                    index={index}
+                    dark={dark}
+                    handleStarred={handleStared}
+                    stared={stared}
+                  />
+                ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default DataTable;
--- src/Components/DataTable/TableRow.js
+++ src/Components/DataTable/TableRow.js
@@ -1,51 +1,85 @@
-import React, { useEffect } from 'react';
-import toast, { Toaster, useToasterStore } from 'react-hot-toast';
-import CopyToClipboard from 'react-copy-to-clipboard';
-import metaMask from '../../images/metamask.svg';
+import React, { useEffect } from "react";
+import toast, { Toaster, useToasterStore } from "react-hot-toast";
+import CopyToClipboard from "react-copy-to-clipboard";
+import metaMask from "../../images/metamask.svg";
 
-const TableRow = ({ data, index, dark, handleStared: handleStarred, stared }) => {
-    const { toasts } = useToasterStore();
+const TableRow = ({ data, index, dark, handleStarred, stared }) => {
+  const { toasts } = useToasterStore();
 
-    const TOAST_LIMIT = 1
+  const TOAST_LIMIT = 1;
 
-    useEffect(() => {
+  useEffect(() => {
     toasts
-        .filter((t) => t.visible) // Only consider visible toasts
-        .filter((_, i) => i >= TOAST_LIMIT) // Is toast index over limit?
-        .forEach((t) => toast.dismiss(t.id)); // Dismiss – Use toast.remove(t.id) for no exit animation
-    }, [toasts]);
+      .filter((t) => t.visible) // Only consider visible toasts
+      .filter((_, i) => i >= TOAST_LIMIT) // Is toast index over limit?
+      .forEach((t) => toast.dismiss(t.id)); // Dismiss – Use toast.remove(t.id) for no exit animation
+  }, [toasts]);
 
-    // const [copy, setCopy] = useState(false)
+  // const [copy, setCopy] = useState(false)
 
-    const handleCopy  = () => {
-        toast.success('Copied Address: '+ data.address, {
-                duration: 2000,
-                style: {
-                    boxShadow: '0px 0px 4px rgba(110, 99, 71, 0.1)',
-                    color: dark ? '#fff' : '#000',
-                    backgroundColor: dark ? '#1f2937' : '#fff',
-                },
-            }
-        );
-    } 
+  const handleCopy = () => {
+    toast.success("Copied Address: " + data.address, {
+      duration: 2000,
+      style: {
+        boxShadow: "0px 0px 4px rgba(110, 99, 71, 0.1)",
+        color: dark ? "#fff" : "#000",
+        backgroundColor: dark ? "#1f2937" : "#fff",
+      },
+    });
+  };
 
-    const isFound = stared?.find(item => item.id === data.id);
-    const handleRedirect = () => {
-        if(data.detail){
-            window.location=data.detail;
-        }
+  const isFound = stared?.find((item) => item.id === data.id);
+  const handleRedirect = () => {
+    if (data.detail) {
+      window.location = data.detail;
     }
+  };
 
-    return (
-        // <tr className={`hover ${data.detail && "cursor-pointer"} hoverss bg-[#212429]`} onClick={handleRedirect}>
-        <tr className={`hover ${data.detail && "cursor-pointer"} hoverss bg-[#212429]`} onClick={() => null}>
-            <th >
-                <div className='flex items-center cursor-pointer'>
-                    <i className={`${isFound ? "fa-solid" : "fa-regular"} fa-star mr-3`} onClick={() => handleStarred(data)}></i>
-                    {index + 1}
-                </div>
-            </th>
-            <td>
+  return (
+    // <tr className={`hover ${data.detail && "cursor-pointer"} hoverss bg-[#212429]`} onClick={handleRedirect}>
+    <tr
+      className={`hover ${
+        data.detail && "cursor-pointer"
+      } hoverss bg-[#212429]`}
+      onClick={() => null}
+    >
+      <th>
+        <div className="flex items-center cursor-pointer">
+          <i
+            className={`${isFound ? "fa-solid" : "fa-regular"} fa-star mr-3`}
+            onClick={() => handleStarred(data)}
+          ></i>
+          {index + 1}
+        </div>
+      </th>
+      <th className="sticky left-16">
+        <div className="flex items-center min-w-[200px]">
+          <img
+            src={`./images/coins/${data?.coin?.icon}`}
+            className="w-10 h-10 mr-2"
+            alt=""
+          />
+          <div className="ml-5 text-start">
+            <h4 className="text-md mb-1">{data?.coin?.name}</h4>
+            <div className="flex items-center">
+              <a
+                href={data?.buy}
+                className="bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white px-2 border border-green-500 hover:border-transparent rounded text-[12px] mr-1"
+              >
+                BUY
+              </a>
+              <h5
+                className={`text-[10px] px-2 bg-green-100 text-black-800 ${
+                  dark && "bg-gray-900 text-gray-400"
+                } rounded p-1 cn `}
+              >
+                {data?.coin?.shortName}
+              </h5>
+            </div>
+          </div>
+        </div>
+      </th>
+      {/* <td>
                 <div className='flex items-center w-[227px]'>
                     <img src={`./images/coins/${data?.coin?.icon}`} className="w-10 h-10 mr-2" alt="" />
                     <div className='ml-5 text-start'>
@@ -56,41 +90,122 @@ const TableRow = ({ data, index, dark, handleStared: handleStarred, stared }) =>
                         </div>
                     </div>
                 </div>
-            </td>
-            <td>${data?.price}</td>
-            <td>
-                <span className={`px-2 py-1 rounded font-bold ${!dark ? parseFloat(data?.durations[0]) < 0 ? "text-red-600 bg-red-50" : "bg-green-100 text-green-800" : ""} ${dark ? parseFloat(data?.durations[0]) < 0 ? "text-red-600 bg-[#341d2a]" : "bg-[#243335] text-green-400" : ""}`}  >{data?.durations[0]}%</span>
-            </td>
-            <td>
-                <span className={`px-2 py-1 rounded font-bold ${!dark ? parseFloat(data?.durations[1]) < 0 ? "text-red-600 bg-red-50" : "bg-green-100 text-green-800" : ""} ${dark ? parseFloat(data?.durations[1]) < 0 ? "text-red-600 bg-[#341d2a]" : "bg-[#243335] text-green-400" : ""}`} >{data?.durations[1]}%</span>
-            </td>
-            <td>
-                <span className={`px-2 py-1 rounded font-bold ${!dark ? parseFloat(data?.durations[2]) < 0 ? "text-red-600 bg-red-50" : "bg-green-100 text-green-800" : ""} ${dark ? parseFloat(data?.durations[2]) < 0 ? "text-red-600 bg-[#341d2a]" : "bg-[#243335] text-green-400" : ""}`} >{data?.durations[2]}%</span>
-            </td>
-            <td>
-                <span className={`px-2 py-1 rounded font-bold bg-[#222e46] ${!dark && "bg-blue-100 text-blue-900"}`} >${data?.liquidity}</span>
-            </td>
-            <td>${data?.marketCap}</td>
-            <td>
-                {data?.supply}
-            </td>
-            <td>
-                <div className='flex items-center'>
-                    <a href="##">
-                        <img width={18} src={metaMask} alt="metamask"/>
-                    </a>
-                    <CopyToClipboard onCopy={handleCopy} text={data.address} >
-                        <svg className='mx-2 cursor-pointer' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" height="20" width="20"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"></path></svg>
-                    </CopyToClipboard>
-                    <Toaster />
-                
-                    <a href="##">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" height="20" width="20"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path></svg>
-                    </a>
-                </div>
-            </td>
-        </tr>
-    );
+            </td> */}
+      <td>${data?.price}</td>
+      <td>
+        <span
+          className={`px-2 py-1 rounded font-bold ${
+            !dark
+              ? parseFloat(data?.durations[0]) < 0
+                ? "text-red-600 bg-red-50"
+                : "bg-green-100 text-green-800"
+              : ""
+          } ${
+            dark
+              ? parseFloat(data?.durations[0]) < 0
+                ? "text-red-600 bg-[#341d2a]"
+                : "bg-[#243335] text-green-400"
+              : ""
+          }`}
+        >
+          {data?.durations[0]}%
+        </span>
+      </td>
+      <td>
+        <span
+          className={`px-2 py-1 rounded font-bold ${
+            !dark
+              ? parseFloat(data?.durations[1]) < 0
+                ? "text-red-600 bg-red-50"
+                : "bg-green-100 text-green-800"
+              : ""
+          } ${
+            dark
+              ? parseFloat(data?.durations[1]) < 0
+                ? "text-red-600 bg-[#341d2a]"
+                : "bg-[#243335] text-green-400"
+              : ""
+          }`}
+        >
+          {data?.durations[1]}%
+        </span>
+      </td>
+      <td>
+        <span
+          className={`px-2 py-1 rounded font-bold ${
+            !dark
+              ? parseFloat(data?.durations[2]) < 0
+                ? "text-red-600 bg-red-50"
+                : "bg-green-100 text-green-800"
+              : ""
+          } ${
+            dark
+              ? parseFloat(data?.durations[2]) < 0
+                ? "text-red-600 bg-[#341d2a]"
+                : "bg-[#243335] text-green-400"
+              : ""
+          }`}
+        >
+          {data?.durations[2]}%
+        </span>
+      </td>
+      <td>
+        <span
+          className={`px-2 py-1 rounded font-bold bg-[#222e46] ${
+            !dark && "bg-blue-100 text-blue-900"
+          }`}
+        >
+          ${data?.liquidity}
+        </span>
+      </td>
+      <td>${data?.marketCap}</td>
+      <td>{data?.supply}</td>
+      <td>
+        <div className="flex items-center">
+          <a href="##">
+            <img width={18} src={metaMask} alt="metamask" />
+          </a>
+          <CopyToClipboard onCopy={handleCopy} text={data.address}>
+            <svg
+              className="mx-2 cursor-pointer"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              height="20"
+              width="20"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
+              ></path>
+            </svg>
+          </CopyToClipboard>
+          <Toaster />
+
+          <a href="##">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              height="20"
+              width="20"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+              ></path>
+            </svg>
+          </a>
+        </div>
+      </td>
+    </tr>
+  );
 };
 
 export default TableRow;
